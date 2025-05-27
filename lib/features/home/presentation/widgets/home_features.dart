140b01fe2987c6ff521ca5a33cf2d4d1ee21b696
rafiharefa <107272666+rafiharefa@users.noreply.github.com>
2025-05-27 10:55:20 +0700

    kitchen-order initialization

--- lib/features/home/presentation/widgets/home_features.dart
+++ lib/features/home/presentation/widgets/home_features.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rebill_flutter/core/providers/orientation_provider.dart';
+import 'package:rebill_flutter/features/kitchen-order/providers/kitchen_order_provider.dart';
 import 'package:rebill_flutter/features/main_bill/presentations/pages/main_component_page.dart';
 import 'package:rebill_flutter/features/home/presentation/widgets/home_bill.dart';
 import 'package:rebill_flutter/features/home/presentation/widgets/home_products.dart';
+import 'package:rebill_flutter/features/kitchen-order/presentations/widgets/processed_order.dart';
+import 'package:rebill_flutter/features/kitchen-order/presentations/widgets/submitted_order.dart';
+import 'package:rebill_flutter/features/kitchen-order/presentations/widgets/finished_order.dart';
 
 class HomeFeatures extends ConsumerWidget {
   const HomeFeatures({super.key});
@@ -12,17 +16,66 @@ class HomeFeatures extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final isLandscape = ref.watch(orientationProvider);
     final theme = Theme.of(context);
+    final kitchenMode = ref.watch(isKitchenOrderModeProvider);
 
     const homeFeatures = [HomeProducts(), MainPage(), HomeBill()];
+    const kitchenOrderFeatures = [
+      ProcessedOrder(),
+      SubmittedOrder(),
+      FinishedOrder(),
+    ];
 
     if (isLandscape) {
       // Landscape layout with Row
       return Flexible(
         child: Padding(
           padding: const EdgeInsets.only(left: 12, right: 12, bottom: 12),
-          child: Row(
-            spacing: 12,
-            children: homeFeatures.map((e) => Expanded(child: e)).toList(),
+          child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 300),
+            reverseDuration: const Duration(milliseconds: 300),
+            transitionBuilder: (child, animation) {
+              final isForward =
+                  animation.status == AnimationStatus.forward ||
+                  animation.status == AnimationStatus.completed;
+
+              final slideAnimation = Tween<Offset>(
+                begin:
+                    isForward
+                        ? const Offset(-1.0, 0.0)
+                        : const Offset(1.0, 0.0),
+                end: Offset.zero,
+              ).animate(
+                CurvedAnimation(
+                  parent: animation,
+                  curve: Curves.easeInOut,
+                  reverseCurve: Curves.easeInOut,
+                ),
+              );
+
+              return FadeTransition(
+                opacity: CurvedAnimation(
+                  parent: animation,
+                  curve: Curves.easeInOut,
+                ),
+                child: SlideTransition(position: slideAnimation, child: child),
+              );
+            },
+            child:
+                kitchenMode
+                    ? Row(
+                      spacing: 12,
+                      key: const ValueKey<int>(0),
+                      children:
+                          kitchenOrderFeatures
+                              .map((e) => Expanded(child: e))
+                              .toList(),
+                    )
+                    : Row(
+                      spacing: 12,
+                      key: const ValueKey<int>(1),
+                      children:
+                          homeFeatures.map((e) => Expanded(child: e)).toList(),
+                    ),
           ),
         ),
       );
---
+++ lib/features/kitchen-order/presentations/widgets/processed_order.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rebill_flutter/core/theme/app_theme.dart';
+
+class ProcessedOrder extends ConsumerWidget {
+  const ProcessedOrder({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.red,
+        boxShadow: AppTheme.kBoxShadow,
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+  }
+}
