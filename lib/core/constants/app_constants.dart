065a383a184511a027375b10c65e45d9652f30bf
RobbyAhmad <muhamadrobby24@gmail.com>
2025-09-18 10:41:59 +0700

    Update URL

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -7,7 +7,7 @@ class AppConstants {
 
   // Base URL
   static const String baseUrl =
-      'https://rights-color-sleeps-taught.trycloudflare.com';
+      'https://spray-naples-acute-silly.trycloudflare.com';
 
   // Service URL
   static const String service = '/pos/';
